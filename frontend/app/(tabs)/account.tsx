7a6ad5713e53736f0f730618de2872ed9494eb85
unknown <vidutilakarathne@gmail.com>
2026-04-25 22:39:03 +0530

    Add blog management crud

--- frontend/app/(tabs)/account.tsx
+++
@@ -1,2 +0,0 @@
-import AccountScreen from '../account';
-export default AccountScreen;
--- frontend/app/register.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import RegisterScreen from '../src/screens/RegisterScreen';
-
-export default function Register() {
-  return <RegisterScreen />;
-}
